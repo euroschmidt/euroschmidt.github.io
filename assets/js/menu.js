656658320e135426d85f2f8daccf5bced8a8d284
euroschmidt <schmidtevelin94@gmail.com>
2020-09-11 15:15:05 +0200

    Update

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,10 @@
 
 $('.subnav-header').click( function(e) {
     e.preventDefault();
-    console.log(e);
     console.log($(e.target).next('.subnav').toggleClass('visible'))
+});
+
+
+$('.product button').click( function(e) {
+    location.href = 'rendeles.html'
 });
