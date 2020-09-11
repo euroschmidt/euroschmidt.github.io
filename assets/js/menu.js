935d7130b0071597a081507974082f07ddd582ce
euroschmidt <schmidtevelin94@gmail.com>
2020-09-11 15:19:16 +0200

    remove log from click

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,11 @@
 
 $('.subnav-header').click( function(e) {
     e.preventDefault();
-    console.log($(e.target).next('.subnav').toggleClass('visible'))
+    $(e.target).next('.subnav').toggleClass('visible')
 });
 
 
 $('.product button').click( function(e) {
+    e.preventDefault();
     location.href = 'rendeles.html'
 });
